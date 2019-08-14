7b0288e194c328f5a83739ffc1daa575a0da01b2
Mitesh Patel <mitesh@bluegreytech.com>
2019-08-14 04:15:25 -0400

    add

--- html/logs/log-2019-08-14.php
+++ html/logs/log-2019-08-14.php
@@ -85,12 +85,3 @@ ERROR - 2019-08-14 08:02:34 --> Severity: Warning --> include(): Failed opening 
 ERROR - 2019-08-14 08:02:34 --> 404 Page Not Found: Default_2/css
 ERROR - 2019-08-14 08:02:34 --> Severity: Warning --> include(application/views\errors\html\error_404.php): failed to open stream: No such file or directory C:\xampp\htdocs\mentor\system\core\Exceptions.php 182
 ERROR - 2019-08-14 08:02:34 --> Severity: Warning --> include(): Failed opening 'application/views\errors\html\error_404.php' for inclusion (include_path='C:\xampp\php\PEAR') C:\xampp\htdocs\mentor\system\core\Exceptions.php 182
-ERROR - 2019-08-14 09:29:01 --> 404 Page Not Found: Default/images
-ERROR - 2019-08-14 09:29:01 --> Severity: Warning --> include(application/views\errors\html\error_404.php): failed to open stream: No such file or directory C:\xampp\htdocs\mentor\system\core\Exceptions.php 182
-ERROR - 2019-08-14 09:29:01 --> Severity: Warning --> include(): Failed opening 'application/views\errors\html\error_404.php' for inclusion (include_path='C:\xampp\php\PEAR') C:\xampp\htdocs\mentor\system\core\Exceptions.php 182
-ERROR - 2019-08-14 09:29:01 --> 404 Page Not Found: Default/asset
-ERROR - 2019-08-14 09:29:01 --> Severity: Warning --> include(application/views\errors\html\error_404.php): failed to open stream: No such file or directory C:\xampp\htdocs\mentor\system\core\Exceptions.php 182
-ERROR - 2019-08-14 09:29:01 --> Severity: Warning --> include(): Failed opening 'application/views\errors\html\error_404.php' for inclusion (include_path='C:\xampp\php\PEAR') C:\xampp\htdocs\mentor\system\core\Exceptions.php 182
-ERROR - 2019-08-14 09:29:01 --> 404 Page Not Found: Default/asset
-ERROR - 2019-08-14 09:29:01 --> Severity: Warning --> include(application/views\errors\html\error_404.php): failed to open stream: No such file or directory C:\xampp\htdocs\mentor\system\core\Exceptions.php 182
-ERROR - 2019-08-14 09:29:01 --> Severity: Warning --> include(): Failed opening 'application/views\errors\html\error_404.php' for inclusion (include_path='C:\xampp\php\PEAR') C:\xampp\htdocs\mentor\system\core\Exceptions.php 182
