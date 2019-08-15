f684a17f554c7f4ea1165ab5f9477eb21fea7693
veeru225 <52230630+veeru225@users.noreply.github.com>
2019-08-15 01:30:08 -0400

    add

--- logs/log-2019-08-15.php
+++ logs/log-2019-08-15.php
@@ -210,3 +210,9 @@ ERROR - 2019-08-15 07:19:49 --> Severity: Warning --> include(): Failed opening 
 ERROR - 2019-08-15 07:20:03 --> 404 Page Not Found: Default/css
 ERROR - 2019-08-15 07:20:03 --> Severity: Warning --> include(application/views\errors\html\error_404.php): failed to open stream: No such file or directory C:\xampp\htdocs\mentor\system\core\Exceptions.php 182
 ERROR - 2019-08-15 07:20:03 --> Severity: Warning --> include(): Failed opening 'application/views\errors\html\error_404.php' for inclusion (include_path='C:\xampp\php\PEAR') C:\xampp\htdocs\mentor\system\core\Exceptions.php 182
+ERROR - 2019-08-15 07:26:51 --> 404 Page Not Found: Default/images
+ERROR - 2019-08-15 07:26:51 --> Severity: Warning --> include(application/views\errors\html\error_404.php): failed to open stream: No such file or directory C:\xampp\htdocs\mentor\system\core\Exceptions.php 182
+ERROR - 2019-08-15 07:26:51 --> Severity: Warning --> include(): Failed opening 'application/views\errors\html\error_404.php' for inclusion (include_path='C:\xampp\php\PEAR') C:\xampp\htdocs\mentor\system\core\Exceptions.php 182
+ERROR - 2019-08-15 07:27:39 --> 404 Page Not Found: Default/css
+ERROR - 2019-08-15 07:27:39 --> Severity: Warning --> include(application/views\errors\html\error_404.php): failed to open stream: No such file or directory C:\xampp\htdocs\mentor\system\core\Exceptions.php 182
+ERROR - 2019-08-15 07:27:39 --> Severity: Warning --> include(): Failed opening 'application/views\errors\html\error_404.php' for inclusion (include_path='C:\xampp\php\PEAR') C:\xampp\htdocs\mentor\system\core\Exceptions.php 182
