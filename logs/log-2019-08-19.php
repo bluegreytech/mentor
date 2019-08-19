ca591202a84e7d1bc5d4edbb50ee8b989d2ba8e1
Binny29 <52149842+Binny29@users.noreply.github.com>
2019-08-19 11:45:55 +0530

    add

--- logs/log-2019-08-19.php
+++ logs/log-2019-08-19.php
@@ -62,3 +62,42 @@ ERROR - 2019-08-19 07:40:38 --> include(): Failed opening 'footer.php' for inclu
 ERROR - 2019-08-19 07:41:06 --> include(header.php): failed to open stream: No such file or directory at line no 1 in C:\xampp\htdocs\mentor\application\views\classes\mentor_partnership_program.php
 ERROR - 2019-08-19 07:41:06 --> include(): Failed opening 'header.php' for inclusion (include_path='C:\xampp\php\PEAR') at line no 1 in C:\xampp\htdocs\mentor\application\views\classes\mentor_partnership_program.php
 ERROR - 2019-08-19 07:41:37 --> 404 Page Not Found: Default/images
+ERROR - 2019-08-19 07:46:34 --> Undefined variable: UserId at line no 54 in C:\xampp\htdocs\mentor\application\views\Dashboard\Editprofile.php
+ERROR - 2019-08-19 07:47:57 --> Undefined variable: UserId at line no 54 in C:\xampp\htdocs\mentor\application\views\Dashboard\Editprofile.php
+ERROR - 2019-08-19 07:48:55 --> 404 Page Not Found: Default_2/js
+ERROR - 2019-08-19 07:50:01 --> Undefined variable: UserId at line no 54 in C:\xampp\htdocs\mentor\application\views\Dashboard\Editprofile.php
+ERROR - 2019-08-19 07:50:03 --> 404 Page Not Found: Default_2/js
+ERROR - 2019-08-19 07:50:29 --> Undefined variable: UserId at line no 54 in C:\xampp\htdocs\mentor\application\views\Dashboard\Editprofile.php
+ERROR - 2019-08-19 07:50:31 --> 404 Page Not Found: Default_2/js
+ERROR - 2019-08-19 07:50:31 --> Undefined variable: UserId at line no 54 in C:\xampp\htdocs\mentor\application\views\Dashboard\Editprofile.php
+ERROR - 2019-08-19 07:50:32 --> 404 Page Not Found: Default_2/js
+ERROR - 2019-08-19 07:54:45 --> Undefined variable: UserId at line no 54 in C:\xampp\htdocs\mentor\application\views\Dashboard\Editprofile.php
+ERROR - 2019-08-19 07:54:46 --> 404 Page Not Found: Default_2/js
+ERROR - 2019-08-19 07:55:03 --> Undefined variable: UserId at line no 54 in C:\xampp\htdocs\mentor\application\views\Dashboard\Editprofile.php
+ERROR - 2019-08-19 07:55:22 --> 404 Page Not Found: Default_2/js
+ERROR - 2019-08-19 07:55:35 --> Undefined variable: UserId at line no 54 in C:\xampp\htdocs\mentor\application\views\Dashboard\Editprofile.php
+ERROR - 2019-08-19 07:55:36 --> 404 Page Not Found: Default_2/js
+ERROR - 2019-08-19 07:57:06 --> Undefined variable: UserId at line no 54 in C:\xampp\htdocs\mentor\application\views\Dashboard\Editprofile.php
+ERROR - 2019-08-19 07:57:07 --> 404 Page Not Found: Default_2/js
+ERROR - 2019-08-19 08:00:22 --> Undefined variable: UserId at line no 54 in C:\xampp\htdocs\mentor\application\views\Dashboard\Editprofile.php
+ERROR - 2019-08-19 08:00:22 --> Undefined variable: GalleryImage at line no 193 in C:\xampp\htdocs\mentor\application\views\Dashboard\Editprofile.php
+ERROR - 2019-08-19 08:00:23 --> 404 Page Not Found: Default_2/js
+ERROR - 2019-08-19 08:01:49 --> Undefined variable: UserId at line no 54 in C:\xampp\htdocs\mentor\application\views\Dashboard\Editprofile.php
+ERROR - 2019-08-19 08:01:50 --> Undefined variable: GalleryImage at line no 196 in C:\xampp\htdocs\mentor\application\views\Dashboard\Editprofile.php
+ERROR - 2019-08-19 08:01:51 --> 404 Page Not Found: Default_2/js
+ERROR - 2019-08-19 08:01:58 --> Undefined variable: UserId at line no 54 in C:\xampp\htdocs\mentor\application\views\Dashboard\Editprofile.php
+ERROR - 2019-08-19 08:01:58 --> Undefined variable: GalleryImage at line no 196 in C:\xampp\htdocs\mentor\application\views\Dashboard\Editprofile.php
+ERROR - 2019-08-19 08:01:59 --> 404 Page Not Found: Default_2/js
+ERROR - 2019-08-19 08:03:44 --> Undefined variable: UserId at line no 54 in C:\xampp\htdocs\mentor\application\views\Dashboard\Editprofile.php
+ERROR - 2019-08-19 08:03:44 --> Undefined variable: GalleryImage at line no 196 in C:\xampp\htdocs\mentor\application\views\Dashboard\Editprofile.php
+ERROR - 2019-08-19 08:04:04 --> Undefined variable: UserId at line no 54 in C:\xampp\htdocs\mentor\application\views\Dashboard\Editprofile.php
+ERROR - 2019-08-19 08:04:04 --> Undefined variable: GalleryImage at line no 196 in C:\xampp\htdocs\mentor\application\views\Dashboard\Editprofile.php
+ERROR - 2019-08-19 08:05:24 --> Undefined variable: UserId at line no 54 in C:\xampp\htdocs\mentor\application\views\Dashboard\Editprofile.php
+ERROR - 2019-08-19 08:05:24 --> Undefined variable: GalleryImage at line no 196 in C:\xampp\htdocs\mentor\application\views\Dashboard\Editprofile.php
+ERROR - 2019-08-19 08:07:08 --> Undefined variable: GalleryImage at line no 196 in C:\xampp\htdocs\mentor\application\views\Dashboard\Editprofile.php
+ERROR - 2019-08-19 08:08:14 --> Undefined variable: GalleryImage at line no 196 in C:\xampp\htdocs\mentor\application\views\Dashboard\Editprofile.php
+ERROR - 2019-08-19 08:10:21 --> Undefined variable: GalleryImage at line no 196 in C:\xampp\htdocs\mentor\application\views\Dashboard\Editprofile.php
+ERROR - 2019-08-19 08:11:12 --> Undefined variable: GalleryImage at line no 200 in C:\xampp\htdocs\mentor\application\views\Dashboard\Editprofile.php
+ERROR - 2019-08-19 08:11:17 --> Undefined variable: GalleryImage at line no 200 in C:\xampp\htdocs\mentor\application\views\Dashboard\Editprofile.php
+ERROR - 2019-08-19 08:11:23 --> Undefined variable: GalleryImage at line no 200 in C:\xampp\htdocs\mentor\application\views\Dashboard\Editprofile.php
+ERROR - 2019-08-19 08:12:54 --> Undefined variable: GalleryImage at line no 200 in C:\xampp\htdocs\mentor\application\views\Dashboard\Editprofile.php
