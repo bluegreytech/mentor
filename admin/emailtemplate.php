153706caf21c2978a974db94c4cb04f9acaaf663
veeru225 <52230630+veeru225@users.noreply.github.com>
2019-09-03 04:41:21 -0400

    add

--- admin/emailtemplate.php
+++ admin/emailtemplate.php
@@ -4,13 +4,22 @@
     <head>
           <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <title>Welcome to {site_name}</title>
-       
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
+      <style type="text/css">
+        td{
+          padding: 20px;
+          font-family: Open Sans;
+        }
+        td span{
+          font-weight: 600;
+        }
+      </style>
     </head>
     <body>
       <center>
-         <table cellspacing="0" style="border: 2px; width: 500px; height: 70px;">
+         <table cellspacing="0" style="border: 2px; width: 500px; height: 70px;text-align: center;">
             <tr>
-                <td style="background: #eee; border-bottom: 1px solid #bb342f;"><img src="http://nyalkaran.bluegreytech.co.in/admin/default/images/logo/logo.png" alt="Logo" style="width: 121px;"></td>
+                <td style="background: #eee; border-bottom: 2px solid #bb342f;"><img src="default/images/logo/logo.png" alt="Logo"></td>
               
             </tr>
            
@@ -19,7 +28,7 @@
       
         <table cellspacing="0" style="border: 2px; width: 500px; height: 400px;background: #eee;">
             <tr>
-                <td>Hello {username},</td>
+                <td>Hello <span>{username}</span>,</td>
             </tr>
             <tr >
                <td><p>We were told that you forgot your password on {username}.</p>
@@ -29,13 +38,13 @@
                </td>
             </tr>
             <tr>
-               <td><p>Regard,</p>
-               <p>Nyalkaran Group,</p></td>
+               <td><p><span>Regard,</span></p>
+               <p><span>TogehterMentor</span></p></td>
             </tr>
         </table>
         <table cellspacing="0" style="border: 2px; width: 500px; height: 50px;">
             <tr>
-                <td style="background: linear-gradient(to right, #b31217, #e52d27);"><p style="color: #fff; text-align: center;">Copyright © {year} Nyalkaran Group. All Rights Reserved</p></td>
+                <td style="background: linear-gradient(to right, #b31217, #e52d27);"><p style="color: #fff; text-align: center;">Copyright © <span>{year}</span> Nyalkaran Group. All Rights Reserved</p></td>
               
             </tr>
            
