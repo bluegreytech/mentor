d89511f2995935837dd5b2ee6509d7c1132bcf77
veeru225 <52230630+veeru225@users.noreply.github.com>
2019-09-03 01:36:09 -0400

    add

---
+++ logs/log-2019-09-03.php
@@ -0,0 +1,5 @@
+ERROR - 2019-09-03 07:31:35 --> 404 Page Not Found: Default/images
+ERROR - 2019-09-03 07:31:46 --> 404 Page Not Found: Home/img
+ERROR - 2019-09-03 07:31:46 --> 404 Page Not Found: Default/images
+ERROR - 2019-09-03 07:31:46 --> 404 Page Not Found: Home/img
+ERROR - 2019-09-03 07:31:46 --> 404 Page Not Found: Home/img
