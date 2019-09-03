db5bc120ba7ea87c62f6c29e2c0c5def47aa87a6
veeru225 <52230630+veeru225@users.noreply.github.com>
2019-09-03 02:51:56 -0400

    add

--- admin/application/views/common/login.php
+++ admin/application/views/common/login.php
@@ -2,7 +2,7 @@
 <body class="main-bg">
  
   <div class="app-content content container-fluid">
-      <div class="content-wrapper">
+      <div class="content-wrapper" style="padding-top: 80px;">
         <div class="content-header row">
         </div>
         <div class="content-body"><section class="flexbox-container">
---
+++ logs/log-2019-09-03.php
@@ -0,0 +1,55 @@
+ERROR - 2019-09-03 08:15:15 --> 404 Page Not Found: Default/images
+ERROR - 2019-09-03 08:15:44 --> 404 Page Not Found: Default/images
+ERROR - 2019-09-03 08:15:56 --> 404 Page Not Found: Default/images
+ERROR - 2019-09-03 08:16:21 --> 404 Page Not Found: Default/images
+ERROR - 2019-09-03 08:16:34 --> Undefined variable: activeTab at line no 30 in C:\xampp\htdocs\mentor\application\views\common\sidebar_second.php
+ERROR - 2019-09-03 08:16:34 --> Undefined variable: activeTab at line no 32 in C:\xampp\htdocs\mentor\application\views\common\sidebar_second.php
+ERROR - 2019-09-03 08:16:34 --> Undefined variable: activeTab at line no 34 in C:\xampp\htdocs\mentor\application\views\common\sidebar_second.php
+ERROR - 2019-09-03 08:16:34 --> Undefined variable: activeTab at line no 37 in C:\xampp\htdocs\mentor\application\views\common\sidebar_second.php
+ERROR - 2019-09-03 08:16:54 --> Undefined variable: activeTab at line no 30 in C:\xampp\htdocs\mentor\application\views\common\sidebar_second.php
+ERROR - 2019-09-03 08:16:54 --> Undefined variable: activeTab at line no 32 in C:\xampp\htdocs\mentor\application\views\common\sidebar_second.php
+ERROR - 2019-09-03 08:16:54 --> Undefined variable: activeTab at line no 34 in C:\xampp\htdocs\mentor\application\views\common\sidebar_second.php
+ERROR - 2019-09-03 08:16:54 --> Undefined variable: activeTab at line no 37 in C:\xampp\htdocs\mentor\application\views\common\sidebar_second.php
+ERROR - 2019-09-03 08:17:04 --> Undefined variable: activeTab at line no 30 in C:\xampp\htdocs\mentor\application\views\common\sidebar_second.php
+ERROR - 2019-09-03 08:17:04 --> Undefined variable: activeTab at line no 32 in C:\xampp\htdocs\mentor\application\views\common\sidebar_second.php
+ERROR - 2019-09-03 08:17:04 --> Undefined variable: activeTab at line no 34 in C:\xampp\htdocs\mentor\application\views\common\sidebar_second.php
+ERROR - 2019-09-03 08:17:04 --> Undefined variable: activeTab at line no 37 in C:\xampp\htdocs\mentor\application\views\common\sidebar_second.php
+ERROR - 2019-09-03 08:17:15 --> 404 Page Not Found: Dashboard/Profileedit
+ERROR - 2019-09-03 08:17:17 --> Undefined variable: activeTab at line no 30 in C:\xampp\htdocs\mentor\application\views\common\sidebar_second.php
+ERROR - 2019-09-03 08:17:17 --> Undefined variable: activeTab at line no 32 in C:\xampp\htdocs\mentor\application\views\common\sidebar_second.php
+ERROR - 2019-09-03 08:17:17 --> Undefined variable: activeTab at line no 34 in C:\xampp\htdocs\mentor\application\views\common\sidebar_second.php
+ERROR - 2019-09-03 08:17:17 --> Undefined variable: activeTab at line no 37 in C:\xampp\htdocs\mentor\application\views\common\sidebar_second.php
+ERROR - 2019-09-03 08:17:33 --> Undefined variable: activeTab at line no 30 in C:\xampp\htdocs\mentor\application\views\common\sidebar_second.php
+ERROR - 2019-09-03 08:17:33 --> Undefined variable: activeTab at line no 32 in C:\xampp\htdocs\mentor\application\views\common\sidebar_second.php
+ERROR - 2019-09-03 08:17:34 --> Undefined variable: activeTab at line no 34 in C:\xampp\htdocs\mentor\application\views\common\sidebar_second.php
+ERROR - 2019-09-03 08:17:34 --> Undefined variable: activeTab at line no 37 in C:\xampp\htdocs\mentor\application\views\common\sidebar_second.php
+ERROR - 2019-09-03 08:17:38 --> Undefined variable: activeTab at line no 30 in C:\xampp\htdocs\mentor\application\views\common\sidebar_second.php
+ERROR - 2019-09-03 08:17:38 --> Undefined variable: activeTab at line no 32 in C:\xampp\htdocs\mentor\application\views\common\sidebar_second.php
+ERROR - 2019-09-03 08:17:38 --> Undefined variable: activeTab at line no 34 in C:\xampp\htdocs\mentor\application\views\common\sidebar_second.php
+ERROR - 2019-09-03 08:17:38 --> Undefined variable: activeTab at line no 37 in C:\xampp\htdocs\mentor\application\views\common\sidebar_second.php
+ERROR - 2019-09-03 08:17:41 --> Undefined variable: activeTab at line no 30 in C:\xampp\htdocs\mentor\application\views\common\sidebar_second.php
+ERROR - 2019-09-03 08:17:42 --> Undefined variable: activeTab at line no 32 in C:\xampp\htdocs\mentor\application\views\common\sidebar_second.php
+ERROR - 2019-09-03 08:17:42 --> Undefined variable: activeTab at line no 34 in C:\xampp\htdocs\mentor\application\views\common\sidebar_second.php
+ERROR - 2019-09-03 08:17:42 --> Undefined variable: activeTab at line no 37 in C:\xampp\htdocs\mentor\application\views\common\sidebar_second.php
+ERROR - 2019-09-03 08:17:49 --> Undefined variable: activeTab at line no 30 in C:\xampp\htdocs\mentor\application\views\common\sidebar_second.php
+ERROR - 2019-09-03 08:17:49 --> Undefined variable: activeTab at line no 32 in C:\xampp\htdocs\mentor\application\views\common\sidebar_second.php
+ERROR - 2019-09-03 08:17:49 --> Undefined variable: activeTab at line no 34 in C:\xampp\htdocs\mentor\application\views\common\sidebar_second.php
+ERROR - 2019-09-03 08:17:49 --> Undefined variable: activeTab at line no 37 in C:\xampp\htdocs\mentor\application\views\common\sidebar_second.php
+ERROR - 2019-09-03 08:18:05 --> 404 Page Not Found: Default/images
+ERROR - 2019-09-03 08:18:26 --> Undefined variable: activeTab at line no 30 in C:\xampp\htdocs\mentor\application\views\common\sidebar_second.php
+ERROR - 2019-09-03 08:18:26 --> Undefined variable: activeTab at line no 32 in C:\xampp\htdocs\mentor\application\views\common\sidebar_second.php
+ERROR - 2019-09-03 08:18:26 --> Undefined variable: activeTab at line no 34 in C:\xampp\htdocs\mentor\application\views\common\sidebar_second.php
+ERROR - 2019-09-03 08:18:26 --> Undefined variable: activeTab at line no 37 in C:\xampp\htdocs\mentor\application\views\common\sidebar_second.php
+ERROR - 2019-09-03 08:18:30 --> 404 Page Not Found: Default/images
+ERROR - 2019-09-03 08:18:44 --> 404 Page Not Found: Default/images
+ERROR - 2019-09-03 08:19:07 --> Undefined variable: activeTab at line no 30 in C:\xampp\htdocs\mentor\application\views\common\sidebar_second.php
+ERROR - 2019-09-03 08:19:07 --> Undefined variable: activeTab at line no 32 in C:\xampp\htdocs\mentor\application\views\common\sidebar_second.php
+ERROR - 2019-09-03 08:19:07 --> Undefined variable: activeTab at line no 34 in C:\xampp\htdocs\mentor\application\views\common\sidebar_second.php
+ERROR - 2019-09-03 08:19:07 --> Undefined variable: activeTab at line no 37 in C:\xampp\htdocs\mentor\application\views\common\sidebar_second.php
+ERROR - 2019-09-03 08:19:09 --> 404 Page Not Found: Default/images
+ERROR - 2019-09-03 08:20:06 --> Undefined variable: activeTab at line no 30 in C:\xampp\htdocs\mentor\application\views\common\sidebar_second.php
+ERROR - 2019-09-03 08:20:06 --> Undefined variable: activeTab at line no 32 in C:\xampp\htdocs\mentor\application\views\common\sidebar_second.php
+ERROR - 2019-09-03 08:20:06 --> Undefined variable: activeTab at line no 34 in C:\xampp\htdocs\mentor\application\views\common\sidebar_second.php
+ERROR - 2019-09-03 08:20:06 --> Undefined variable: activeTab at line no 37 in C:\xampp\htdocs\mentor\application\views\common\sidebar_second.php
+ERROR - 2019-09-03 08:22:17 --> 404 Page Not Found: Default/images
+ERROR - 2019-09-03 08:51:07 --> 404 Page Not Found: Default/images
