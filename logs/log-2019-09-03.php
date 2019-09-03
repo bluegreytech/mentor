8ca23e5d5396fc0202d8b342e982dadd23b852b4
Binny29 <52149842+Binny29@users.noreply.github.com>
2019-09-03 17:03:18 +0530

    add

---
+++ logs/log-2019-09-03.php
@@ -0,0 +1,98 @@
+ERROR - 2019-09-03 13:20:41 --> mail(): Failed to connect to mailserver at &quot;localhost&quot; port 25, verify your &quot;SMTP&quot; and &quot;smtp_port&quot; setting in php.ini or use ini_set() at line no 1902 in C:\xampp\htdocs\mentor\system\libraries\Email.php
+ERROR - 2019-09-03 13:20:58 --> 404 Page Not Found: Default/css
+ERROR - 2019-09-03 13:21:01 --> 404 Page Not Found: Default/css
+ERROR - 2019-09-03 13:21:06 --> 404 Page Not Found: Default/css
+ERROR - 2019-09-03 13:21:34 --> 404 Page Not Found: Default/css
+ERROR - 2019-09-03 13:21:40 --> 404 Page Not Found: Default/css
+ERROR - 2019-09-03 13:21:43 --> 404 Page Not Found: Default/css
+ERROR - 2019-09-03 13:21:46 --> 404 Page Not Found: Success/index
+ERROR - 2019-09-03 13:21:50 --> 404 Page Not Found: Default/css
+ERROR - 2019-09-03 13:21:54 --> 404 Page Not Found: Success/index
+ERROR - 2019-09-03 13:21:56 --> 404 Page Not Found: Default/css
+ERROR - 2019-09-03 13:21:58 --> Query error: Table 'mentordb.tblblogs' doesn't exist - Invalid query: SELECT `t1`.*, `t2`.*
+FROM `tblblogs` as `t1`
+LEFT JOIN `tblblogstatus` as `t2` ON `t1`.`BlogStatusId` = `t2`.`BlogStatusId`
+WHERE `BlogStatus` = 'Most Popular'
+ORDER BY `t1`.`BlogId` DESC
+ LIMIT 3
+ERROR - 2019-09-03 13:22:09 --> 404 Page Not Found: Default/css
+ERROR - 2019-09-03 13:22:16 --> 404 Page Not Found: Default/css
+ERROR - 2019-09-03 13:22:18 --> 404 Page Not Found: Default/images
+ERROR - 2019-09-03 13:22:19 --> 404 Page Not Found: Default/images
+ERROR - 2019-09-03 13:22:19 --> 404 Page Not Found: Default/css
+ERROR - 2019-09-03 13:23:13 --> 404 Page Not Found: Default/images
+ERROR - 2019-09-03 13:23:13 --> 404 Page Not Found: Default/css
+ERROR - 2019-09-03 13:23:42 --> 404 Page Not Found: Default/images
+ERROR - 2019-09-03 13:23:42 --> 404 Page Not Found: Default/css
+ERROR - 2019-09-03 13:24:10 --> 404 Page Not Found: Default/images
+ERROR - 2019-09-03 13:24:10 --> 404 Page Not Found: Default/css
+ERROR - 2019-09-03 13:24:20 --> 404 Page Not Found: Default/css
+ERROR - 2019-09-03 13:24:20 --> 404 Page Not Found: Default/images
+ERROR - 2019-09-03 13:24:27 --> 404 Page Not Found: Default/images
+ERROR - 2019-09-03 13:24:27 --> 404 Page Not Found: Default/css
+ERROR - 2019-09-03 13:24:55 --> 404 Page Not Found: Default/images
+ERROR - 2019-09-03 13:24:56 --> 404 Page Not Found: Default/css
+ERROR - 2019-09-03 13:25:12 --> 404 Page Not Found: Default/images
+ERROR - 2019-09-03 13:25:12 --> 404 Page Not Found: Default/css
+ERROR - 2019-09-03 13:25:20 --> 404 Page Not Found: Default/images
+ERROR - 2019-09-03 13:25:40 --> 404 Page Not Found: Default/images
+ERROR - 2019-09-03 13:25:59 --> 404 Page Not Found: Default/images
+ERROR - 2019-09-03 13:27:48 --> 404 Page Not Found: Default/images
+ERROR - 2019-09-03 13:28:06 --> 404 Page Not Found: Default/images
+ERROR - 2019-09-03 13:28:10 --> 404 Page Not Found: Default/css
+ERROR - 2019-09-03 13:28:20 --> Query error: Table 'mentordb.tblblogs' doesn't exist - Invalid query: SELECT `t1`.*, `t2`.*
+FROM `tblblogs` as `t1`
+LEFT JOIN `tblblogstatus` as `t2` ON `t1`.`BlogStatusId` = `t2`.`BlogStatusId`
+WHERE `BlogStatus` = 'Most Popular'
+ORDER BY `t1`.`BlogId` DESC
+ LIMIT 3
+ERROR - 2019-09-03 13:29:07 --> 404 Page Not Found: Default/images
+ERROR - 2019-09-03 13:29:07 --> 404 Page Not Found: Default/css
+ERROR - 2019-09-03 13:29:14 --> 404 Page Not Found: Default/images
+ERROR - 2019-09-03 13:29:14 --> 404 Page Not Found: Default/css
+ERROR - 2019-09-03 13:29:24 --> Query error: Table 'mentordb.tblblogs' doesn't exist - Invalid query: SELECT `t1`.*, `t2`.*
+FROM `tblblogs` as `t1`
+LEFT JOIN `tblblogstatus` as `t2` ON `t1`.`BlogStatusId` = `t2`.`BlogStatusId`
+WHERE `BlogStatus` = 'Most Popular'
+ORDER BY `t1`.`BlogId` DESC
+ LIMIT 3
+ERROR - 2019-09-03 13:29:33 --> Query error: Table 'mentordb.tblblogs' doesn't exist - Invalid query: SELECT `t1`.*, `t2`.*
+FROM `tblblogs` as `t1`
+LEFT JOIN `tblblogstatus` as `t2` ON `t1`.`BlogStatusId` = `t2`.`BlogStatusId`
+WHERE `BlogStatus` = 'Most Popular'
+ORDER BY `t1`.`BlogId` DESC
+ LIMIT 3
+ERROR - 2019-09-03 13:29:35 --> 404 Page Not Found: Default/css
+ERROR - 2019-09-03 13:29:38 --> 404 Page Not Found: Default/css
+ERROR - 2019-09-03 13:29:42 --> 404 Page Not Found: Success/index
+ERROR - 2019-09-03 13:29:47 --> 404 Page Not Found: Default/css
+ERROR - 2019-09-03 13:29:50 --> 404 Page Not Found: Default/images
+ERROR - 2019-09-03 13:29:51 --> 404 Page Not Found: Default/images
+ERROR - 2019-09-03 13:29:51 --> 404 Page Not Found: Default/css
+ERROR - 2019-09-03 13:30:00 --> 404 Page Not Found: Home/img
+ERROR - 2019-09-03 13:30:01 --> 404 Page Not Found: Home/img
+ERROR - 2019-09-03 13:30:01 --> 404 Page Not Found: Home/img
+ERROR - 2019-09-03 13:30:01 --> 404 Page Not Found: Default/images
+ERROR - 2019-09-03 13:30:01 --> 404 Page Not Found: Default/css
+ERROR - 2019-09-03 13:30:10 --> 404 Page Not Found: Home/img
+ERROR - 2019-09-03 13:30:10 --> 404 Page Not Found: Home/img
+ERROR - 2019-09-03 13:30:10 --> 404 Page Not Found: Home/img
+ERROR - 2019-09-03 13:30:10 --> 404 Page Not Found: Default/images
+ERROR - 2019-09-03 13:30:10 --> 404 Page Not Found: Default/css
+ERROR - 2019-09-03 13:30:18 --> 404 Page Not Found: Default/css
+ERROR - 2019-09-03 13:30:29 --> 404 Page Not Found: Default/css
+ERROR - 2019-09-03 13:30:34 --> 404 Page Not Found: Default/css
+ERROR - 2019-09-03 13:30:40 --> 404 Page Not Found: Default/css
+ERROR - 2019-09-03 13:30:49 --> 404 Page Not Found: Default/css
+ERROR - 2019-09-03 13:30:57 --> 404 Page Not Found: Default/css
+ERROR - 2019-09-03 13:31:01 --> 404 Page Not Found: Default/css
+ERROR - 2019-09-03 13:31:06 --> 404 Page Not Found: Default/css
+ERROR - 2019-09-03 13:31:11 --> 404 Page Not Found: Default/css
+ERROR - 2019-09-03 13:31:51 --> 404 Page Not Found: Default/css
+ERROR - 2019-09-03 13:31:55 --> 404 Page Not Found: Default/css
+ERROR - 2019-09-03 13:32:00 --> 404 Page Not Found: Default/css
+ERROR - 2019-09-03 13:32:10 --> 404 Page Not Found: Default/css
+ERROR - 2019-09-03 13:32:18 --> 404 Page Not Found: Default/css
+ERROR - 2019-09-03 13:32:26 --> 404 Page Not Found: Default/css
+ERROR - 2019-09-03 13:32:39 --> 404 Page Not Found: Default/css
+ERROR - 2019-09-03 13:32:55 --> 404 Page Not Found: Default/css
