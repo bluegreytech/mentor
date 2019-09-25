002c6ed87be05c76bb4ba194432f74a21259956d
veeru225 <52230630+veeru225@users.noreply.github.com>
2019-09-25 00:31:40 -0400

    add

---
+++ logs/log-2019-09-25.php
@@ -0,0 +1,7 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2019-09-25 06:29:13 --> 404 Page Not Found: Default/images
+ERROR - 2019-09-25 06:29:39 --> 404 Page Not Found: Default/css
+ERROR - 2019-09-25 06:30:04 --> 404 Page Not Found: Default/css
+ERROR - 2019-09-25 06:30:09 --> 404 Page Not Found: Default/images
+ERROR - 2019-09-25 06:30:16 --> 404 Page Not Found: Default/images
