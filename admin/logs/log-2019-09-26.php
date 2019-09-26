cfe702aa64d9fb04a7ca1419c341ee272d46c225
BINNY A. RAI <binny.rai@TECHGURU.LOCAL>
2019-09-26 01:11:17 -0700

    add

---
+++ admin/logs/log-2019-09-26.php
@@ -0,0 +1,8 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2019-09-26 10:09:59 --> 404 Page Not Found: Favicon/favicon-32.png
+ERROR - 2019-09-26 10:09:59 --> 404 Page Not Found: Favicon/favicon.ico
+ERROR - 2019-09-26 10:10:11 --> 404 Page Not Found: Default/images
+ERROR - 2019-09-26 10:10:11 --> 404 Page Not Found: Default/images
+ERROR - 2019-09-26 10:10:20 --> 404 Page Not Found: Default/images
+ERROR - 2019-09-26 10:10:20 --> 404 Page Not Found: Default/images
---
+++ logs/log-2019-09-26.php
@@ -0,0 +1,4 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2019-09-26 10:09:53 --> 404 Page Not Found: Default/images
+ERROR - 2019-09-26 10:10:40 --> 404 Page Not Found: Default/images
