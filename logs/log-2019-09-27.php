ec9e40d6753e3da5320c62d8cb28bdc9574c6da5
veeru225 <52230630+veeru225@users.noreply.github.com>
2019-09-27 07:08:24 -0400

    add

--- logs/log-2019-09-27.php
+++ logs/log-2019-09-27.php
@@ -48,3 +48,82 @@ FROM `tblblog`
 WHERE `IsActive` = 'Active'
 AND `Is_deleted` = '0'
 ERROR - 2019-09-27 12:41:11 --> 404 Page Not Found: Default/images
+ERROR - 2019-09-27 12:41:42 --> 404 Page Not Found: Default/images
+ERROR - 2019-09-27 12:41:54 --> 404 Page Not Found: Default/images
+ERROR - 2019-09-27 12:41:55 --> 404 Page Not Found: Default/images
+ERROR - 2019-09-27 12:41:56 --> 404 Page Not Found: Default/images
+ERROR - 2019-09-27 12:41:58 --> 404 Page Not Found: Default/images
+ERROR - 2019-09-27 12:42:47 --> 404 Page Not Found: Default/images
+ERROR - 2019-09-27 12:42:54 --> 404 Page Not Found: Default/images
+ERROR - 2019-09-27 12:42:57 --> 404 Page Not Found: Default/images
+ERROR - 2019-09-27 12:43:12 --> 404 Page Not Found: Default/css
+ERROR - 2019-09-27 12:43:26 --> 404 Page Not Found: Default/images
+ERROR - 2019-09-27 12:43:36 --> Query error: Table 'mentordb.tblblog' doesn't exist - Invalid query: SELECT *
+FROM `tblblog`
+WHERE `IsActive` = 'Active'
+AND `Is_deleted` = '0'
+ERROR - 2019-09-27 12:43:36 --> 404 Page Not Found: Default/images
+ERROR - 2019-09-27 12:44:10 --> 404 Page Not Found: Default/css
+ERROR - 2019-09-27 12:44:15 --> 404 Page Not Found: Default/css
+ERROR - 2019-09-27 12:44:18 --> 404 Page Not Found: Default/css
+ERROR - 2019-09-27 12:44:22 --> 404 Page Not Found: Default/css
+ERROR - 2019-09-27 12:44:26 --> 404 Page Not Found: Default/css
+ERROR - 2019-09-27 12:44:30 --> 404 Page Not Found: Default/css
+ERROR - 2019-09-27 12:44:45 --> 404 Page Not Found: Default/images
+ERROR - 2019-09-27 12:44:52 --> Query error: Table 'mentordb.tblblog' doesn't exist - Invalid query: SELECT *
+FROM `tblblog`
+WHERE `IsActive` = 'Active'
+AND `Is_deleted` = '0'
+ERROR - 2019-09-27 12:44:52 --> 404 Page Not Found: Default/images
+ERROR - 2019-09-27 12:46:57 --> Query error: Table 'mentordb.tblblog' doesn't exist - Invalid query: SELECT *
+FROM `tblblog`
+WHERE `IsActive` = 'Active'
+AND `Is_deleted` = '0'
+ERROR - 2019-09-27 12:46:57 --> 404 Page Not Found: Default/images
+ERROR - 2019-09-27 12:50:48 --> 404 Page Not Found: Default/images
+ERROR - 2019-09-27 12:52:16 --> Query error: Table 'mentordb.tblblog' doesn't exist - Invalid query: SELECT *
+FROM `tblblog`
+WHERE `IsActive` = 'Active'
+AND `Is_deleted` = '0'
+ERROR - 2019-09-27 12:52:16 --> 404 Page Not Found: Default/images
+ERROR - 2019-09-27 12:53:31 --> 404 Page Not Found: Default/images
+ERROR - 2019-09-27 12:53:31 --> 404 Page Not Found: Default/images
+ERROR - 2019-09-27 12:53:34 --> 404 Page Not Found: Default/images
+ERROR - 2019-09-27 12:53:34 --> 404 Page Not Found: Default/images
+ERROR - 2019-09-27 12:59:53 --> 404 Page Not Found: Default/images
+ERROR - 2019-09-27 12:59:55 --> 404 Page Not Found: Default/images
+ERROR - 2019-09-27 12:59:57 --> 404 Page Not Found: Default/images
+ERROR - 2019-09-27 12:59:57 --> 404 Page Not Found: Default/images
+ERROR - 2019-09-27 12:59:59 --> 404 Page Not Found: Default/images
+ERROR - 2019-09-27 13:00:20 --> 404 Page Not Found: Default/css
+ERROR - 2019-09-27 13:00:39 --> 404 Page Not Found: Default/css
+ERROR - 2019-09-27 13:02:45 --> 404 Page Not Found: Default/images
+ERROR - 2019-09-27 13:02:46 --> Query error: Table 'mentordb.tblblog' doesn't exist - Invalid query: SELECT *
+FROM `tblblog`
+WHERE `IsActive` = 'Active'
+AND `Is_deleted` = '0'
+ERROR - 2019-09-27 13:02:52 --> 404 Page Not Found: Default/css
+ERROR - 2019-09-27 13:03:51 --> 404 Page Not Found: Default/css
+ERROR - 2019-09-27 13:05:23 --> 404 Page Not Found: Default/css
+ERROR - 2019-09-27 13:05:25 --> Query error: Table 'mentordb.tblblog' doesn't exist - Invalid query: SELECT *
+FROM `tblblog`
+WHERE `IsActive` = 'Active'
+AND `Is_deleted` = '0'
+ERROR - 2019-09-27 13:05:25 --> 404 Page Not Found: Default/css
+ERROR - 2019-09-27 13:05:33 --> Query error: Table 'mentordb.tblblog' doesn't exist - Invalid query: SELECT *
+FROM `tblblog`
+WHERE `IsActive` = 'Active'
+AND `Is_deleted` = '0'
+ERROR - 2019-09-27 13:05:33 --> 404 Page Not Found: Default/css
+ERROR - 2019-09-27 13:05:35 --> Query error: Table 'mentordb.tblblog' doesn't exist - Invalid query: SELECT *
+FROM `tblblog`
+WHERE `IsActive` = 'Active'
+AND `Is_deleted` = '0'
+ERROR - 2019-09-27 13:05:35 --> 404 Page Not Found: Default/css
+ERROR - 2019-09-27 13:05:39 --> 404 Page Not Found: Default/css
+ERROR - 2019-09-27 13:05:43 --> 404 Page Not Found: Default/css
+ERROR - 2019-09-27 13:05:46 --> 404 Page Not Found: Default/css
+ERROR - 2019-09-27 13:05:49 --> 404 Page Not Found: Default/css
+ERROR - 2019-09-27 13:05:56 --> 404 Page Not Found: Default/css
+ERROR - 2019-09-27 13:06:06 --> 404 Page Not Found: Default/css
+ERROR - 2019-09-27 13:06:17 --> 404 Page Not Found: Default/images
